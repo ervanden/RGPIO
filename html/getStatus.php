5d20efe7383a23c143743b92663311916829baf6
ervanden <erik.vanden.meersch@telenet.be>
2017-09-25 09:23:09 +0200

    html cleanup scale icons

--- html/getStatus.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-exec("/home/pi/git/RGPIO/run/runTCPClient  server=localhost port=2602 command=status", $lines);
-echo implode("\n",$lines);
-}
-?>
-
